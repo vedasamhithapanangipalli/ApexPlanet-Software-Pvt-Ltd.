8b2bce34812512654ae6d9b64f4905449f2efbe1
Veda Samhitha Panangipalli <91545073+vedasamhithapanangipalli@users.noreply.github.com>
2025-01-07 20:28:32 +0530

    Add files via upload

---
+++ Task-2/ContactForm/script.js
@@ -0,0 +1,22 @@
+function validateForm() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    // Let us check if name is empty
+    if (name === "") {
+        alert("Name is required.");
+        return false;
+    }
+    // Let us validate the email format
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA0-9]{2,6}$/;
+    if (!emailPattern.test(email)) {
+        alert("Please enter a valid email address.");
+        return false;
+    }
+    // Let us Check if message is empty
+    if (message === "") {
+        alert("Message is required.");
+        return false;
+    }
+    return true; // Form is valid(when all 3 fields are entered and also in the correct format)
+}
